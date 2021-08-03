c32dce6f6c2468f7ce42d874522bfffa4c921307
Giorgio Rengucci <4483986@studenti.unige.it>
2021-08-03 14:45:38 +0200

    completati programmi c++

--- sistemi/sistemi.cpp
+++ sistemi/sistemi.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 int main(){
 
+	cout << "\e[1m+============================+\n+ ALAN - Laboratorio Sistemi +\n+============================+\e[0m";
+
 	/* Creazione matrici */
 	// Matrice a1:
 	float tmp1[4][4] = {{3, 1, -1, 0}, {0, 7, -3, 0}, {0, -3, 9, -2}, {0, 0, 4, -10}};
@@ -42,9 +44,11 @@ int main(){
 	for (int i=0; i<np; i++){
 		matrice_p[i] = new float[np];
 		for (int j=0; j<np; j++){
-			matrice_p[i][j] = ((fact(i+j))/(fact(i)*fact(j)));
+			//matrice_p[i][j] = (fact(i+j) / (fact(i)*fact(j))); // svariona per np>7 :(
+			(i==0 || j==0) ? matrice_p[i][j]=1 : matrice_p[i][j] = (matrice_p[i][j-1] + matrice_p[i-1][j]);
 		}
 	}
+	//stampa_matrice(matrice_p, np, np);
 
 	// Matrice t:
 	const int d0 = 6; // ultima cifra matricola
@@ -60,21 +64,96 @@ int main(){
 		}
 	}
 
-	/* es 1 */
+	/********** es 1 *********************************************************/
 	/* Calcolo norma infinito */
+	cout << "\n\n\e[1m*** Es 1 ***\e[0m\n";
 	cout << "Norma infinito matrice a1: ......... " << norma_inf(matrice_a1, na1) << endl;
 	cout << "Norma infinito matrice a2: ......... " << norma_inf(matrice_a2, na2) << endl;
 	cout << "Norma infinito matrice Pascal: ..... " << norma_inf(matrice_p, np) << endl;
 	cout << "Norma infinito matrice tridiagonale: " << norma_inf(matrice_t, nt) << endl;
 
-	stampa_matrice(matrice_p, np);
 
-	/* es 2 */
-	/* Calcolo termine noto `b` con la formula b=Ax dove x è composto da tutti 1*/
-	/* Risoluzione sistema lineare tramite eliminazione di Gauss*/
+	/********** es 2 *********************************************************/
+	cout << "\n\n\e[1m*** Es 2 ***\e[0m\n";
 
-	/* es 3 */
+	/* Matrice A1 */
+	float *ba1 = trova_b(matrice_a1, na1);
+	cout << "Vettore dei termini noti per A1: ";
+	stampa_vettore_trasposto(ba1, na1);
+
+	float *xa1 = gauss_plus(matrice_a1, ba1, na1);
+	cout << "Vettore soluzioni per A1: ...... ";
+	stampa_vettore_trasposto(xa1, na1);
+	
+	
+	/* Matrice A2 */
+	float *ba2 = trova_b(matrice_a2, na2);
+	cout << "\nVettore dei termini noti per A2: "; 
+	stampa_vettore_trasposto(ba2, na2);
+
+	float *xa2 = gauss_plus(matrice_a2, ba2, na2);
+	cout << "Vettore soluzioni per A2: ...... ";
+	stampa_vettore_trasposto(xa2, na2);
+
+	/* Matrice P */
+	float *bp = trova_b(matrice_p, np);
+	cout << "\nVettore dei termini noti per matr. di Pascal: "; 
+	stampa_vettore_trasposto(bp, np);
+
+	float *xp = gauss_plus(matrice_p, bp, np);
+	cout << "Vettore soluzioni per P: .................... ";
+	stampa_vettore_trasposto(xp, np);
+
+	/* Matrice T */
+	float *bt = trova_b(matrice_t, nt);
+	cout << "\nVettore dei termini noti per matr. tridiagonale: "; 
+	stampa_vettore_trasposto(bt, nt);
+
+	float *xt = gauss_plus(matrice_t, bt, nt);
+	cout << "Vettore soluzioni per T: ....................... ";
+	stampa_vettore_trasposto(xt, nt);
+
+
+	/********** es 3 *********************************************************/
+	cout << "\n\n\e[1m*** Es 3 ***\e[0m\n";
 	/* Risoluzione sistema lineare con termini noti perturbati */
+
+	/* Matrice A1 */
+	float *b_pert_a1 = perturba_b(ba1, na1);
+	cout << "Vettore dei termini noti per A1: ";
+	stampa_vettore_trasposto(b_pert_a1, na1);
+
+	xa1 = gauss_plus(matrice_a1, b_pert_a1, na1);
+	cout << "Vettore soluzioni per A1: ...... ";
+	stampa_vettore_trasposto(xa1, na1);
+	
+	
+	/* Matrice A2 */
+	float *b_pert_a2 = perturba_b(ba2, na2);
+	cout << "\nVettore dei termini noti per A2: "; 
+	stampa_vettore_trasposto(b_pert_a2, na2);
+
+	xa2 = gauss_plus(matrice_a2, b_pert_a2, na2);
+	cout << "Vettore soluzioni per A2: ...... ";
+	stampa_vettore_trasposto(xa2, na2);
+
+	/* Matrice P */
+	float *b_pert_p = perturba_b(bp, np);
+	cout << "\nVettore dei termini noti per matr. di Pascal: "; 
+	stampa_vettore_trasposto(b_pert_p, np);
+
+	xp = gauss_plus(matrice_p, b_pert_p, np);
+	cout << "Vettore soluzioni per P: .................... ";
+	stampa_vettore_trasposto(xp, np);
+
+	/* Matrice T */
+	float *b_pert_t = perturba_b(bt, nt);
+	cout << "\nVettore dei termini noti per matr. tridiagonale: "; 
+	stampa_vettore_trasposto(b_pert_t, nt);
+
+	xt = gauss_plus(matrice_t, b_pert_t, nt);
+	cout << "Vettore soluzioni per T: ....................... ";
+	stampa_vettore_trasposto(xt, nt);
 
 	return 0;
 }
--- sistemi/support.h
+++ sistemi/support.h
@@ -3,14 +3,14 @@
 using namespace std;
 
 /* Funzione fattoriale */
-float fact(float n) { return (n==0) || (n==1) ? 1 : n * fact(n-1); }
+float fact(float n) { return (n<=1) ? 1 : n * fact(n-1); }
 
 /* Funzione calcolo norma infinita */
 float norma_inf(float *matrice[], int n) {
 	float norma = 0;
 	float tmp = 0;
-	for (int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
+	for (unsigned int i=0; i<n; i++){
+		for (unsigned int j=0; j<n; j++){
 			tmp += fabs(matrice[i][j]);
 		}
 		if (tmp > norma){ norma = tmp;}
@@ -19,15 +19,98 @@ float norma_inf(float *matrice[], int n) {
 	return norma;
 }
 
+float norma_inf_vect(float *vettore, int dim){
+	float norma = 0;
+	float tmp = 0;
+	for (unsigned int i=0; i<dim; i++){
+		tmp = fabs(vettore[i]);
+		if (tmp>norma){
+			norma = tmp;
+		}
+	}
+	return norma;
+}
+
 /* PrettyPrint matrice */
-void stampa_matrice(float *matrice[], int n){
-	for (int i=0; i<n; i++){
+void stampa_matrice(float *matrice[], int n1, int n2){
+	for (int i=0; i<n1; i++){
 		cout << "{ ";
-		for (int j=0; j<n; j++){
+		for (int j=0; j<n2; j++){
 			if (matrice[i][j]<0) { cout << matrice[i][j]; }
 			else { cout << " " << matrice[i][j]; }
-			if (j+1!=n) { cout << ", "; }// inserimento virgola ','
+			if (j+1!=n1) { cout << ", "; }// inserimento virgola ','
 		}
 		cout << " }\n";
 	}
+}
+
+/* Stampa del vettore */
+void stampa_vettore(float vettore[], int dim)
+{ for (int i=0; i<dim; i++) { cout << "[" << vettore[i] << "]\n"; }}
+
+/* Stampa del vettore in orizzontale [a1, a2, ..., an] */
+void stampa_vettore_trasposto(float vettore[], int dim)
+{ for (int i=0; i<dim; i++) { cout << (i==0 ? "[" : "") << vettore[i] << (i+1<dim ? ", " : "]\n"); }}
+
+/* Funzione che moltiplica la matrice per il vettore composto da tutti '1' */
+// attenzione: funziona solo con matrici quadrate
+// ritorna il vettore calcolato
+float* trova_b(float *matrice[], int dim){
+	//cout << "\ndim:"<<dim;
+	float *b = new float[dim];
+	float elem = 0;
+	for (unsigned int i=0; i<dim; i++){
+		for (unsigned int j=0; j<dim; j++){
+			elem += matrice[i][j];
+		}
+		//cout << "\nb["<<i<<"] = "<<elem;
+		b[i] = elem;
+		elem = 0;
+	}
+	return b;
+}
+
+/* Perturba il vettore */
+float* perturba_b(float *vettore, int dim){
+	float *b_perturbato = vettore;
+	float n_inf = norma_inf_vect(vettore, dim);
+
+	for (unsigned int i=0; i<dim; i++){
+		b_perturbato[i] += (n_inf * (i%2==0 ? -0.01 : 0.01));
+	}
+	return b_perturbato;
+}
+
+/* Eliminazione gaussiana */
+float* gauss_plus(float** matrice, float* vettore, int dim){
+	float* soluzioni = new float[dim]; // vettore delle soluzioni
+
+	float mat[dim][dim+1]; // matrice composta dalla matrice originale e vettore dei termini noti
+	for (int i=0; i<dim; i++){
+		for (int j=0; j<dim+1; j++){
+			j<dim ? mat[i][j] = matrice[i][j] : mat[i][j] = vettore[i];
+		}
+	}
+
+	for (int i=0; i<dim-1; i++){
+		for (int j=i+1; j<dim; j++){
+			float f=mat[j][i]/mat[i][i];
+			for (int k=0; k<dim+1; k++){
+				mat[j][k] = mat[j][k]-f*mat[i][k];
+			}
+		}
+	}
+	// scorrimento indietro per trovare la soluzione
+	for (int i=dim-1; i>=0; i--){
+		soluzioni[i]=mat[i][dim];
+		for (int j=i+1; j<dim ;j++){
+			if (i!=j){
+				soluzioni[i]=soluzioni[i]-mat[i][j]*soluzioni[j];
+			}
+		}
+		soluzioni[i]=soluzioni[i]/mat[i][i];
+	}
+
+	//stampa_vettore_trasposto(soluzioni, dim);
+	return soluzioni;
 }
